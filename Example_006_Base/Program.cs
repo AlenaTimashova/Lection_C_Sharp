8dca9b7a3ae34866f92fad8c8a4437ad379b88e8
Alena <alyona8808@mail.ru>
2022-10-12 13:31:20 +0300

    Base

---
+++ Example_006_Base/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 3;
+int b = 8;
+int c = 1;
+int d = 4;
+int e = 2;
+
+int max = a;
+
+if (a > max) {
+    max = a;
+}
+if (b > max) {
+    max = b;
+}
+if (c > max) {
+    max = c;
+}
+if (d > max) {
+    max = d;
+}
+if (e > max) {
+    max = e;
+}
+Console.Write ("max = ");
+Console.WriteLine(max);
